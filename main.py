40452f6960c03f320e647459ef6f672efff93ebe
Trinnik <trinnik@trinnik.com>
2024-05-15 10:21:31 -0600

    add book report

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 def main():
-    file_contents = get_book_text("books/frankenstein.txt")
+    book_path = "books/frankenstein.txt"
+    file_contents = get_book_text(book_path)
+    char_count_list = convert_dict_list(get_character_count(file_contents))
+    char_count_list.sort(reverse=True, key=sort_on)
 
-    print(get_character_count(file_contents))
+    generate_char_report(char_count_list, book_path)
+
+
+def generate_char_report(l:list, path: str):
+    print(f"--- Begin report of {path} ---")
+    for i in l:
+        print(f"The '{i["char"]}' character was found {i["num"]} times.")
+    print(f"--- End report ---")
 
 
 def get_character_count(text):
@@ -15,6 +25,21 @@ def get_character_count(text):
             character_dict[i_lower] = 1
 
     return character_dict
+
+
+def convert_dict_list(d: dict):
+    dict_list = []
+
+    for i in d:
+        if i.isalpha():
+            new_dict = {"char": i, "num": d[i]}
+            dict_list.append(new_dict)
+
+    return dict_list
+
+
+def sort_on(dict):
+    return dict["num"]
 
 
 def get_num_words(text):
